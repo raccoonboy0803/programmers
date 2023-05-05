e9ba34d3fa8436bdb3a20ed1ef0bdcf7211302b7
raccoonboy <raccoonboy0803@gmail.com>
2023-05-06 02:42:07 +0900

    #23.05.05 level-0 commit

---
+++ level-0(2)/181905.js
@@ -0,0 +1,16 @@
+// 문자열 뒤집기
+// 문제 설명
+// 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// my_string은 숫자와 알파벳으로만 이루어져 있습니다.
+// 1 ≤ my_string의 길이 ≤ 1,000
+// 0 ≤ s ≤ e < my_string의 길이
+
+function solution(my_string, s, e) {
+  const splitArr = my_string.split('');
+  const reverseArr = splitArr.slice(s, e + 1).reverse();
+  splitArr.splice(s, e - s + 1, ...reverseArr);
+  return splitArr.join('');
+}
+console.log(solution('asdef', 1, 4));
