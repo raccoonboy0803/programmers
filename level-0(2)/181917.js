0ee277d4cd8b27189d16004038df4b247d489401
raccoonboy <raccoonboy0803@gmail.com>
2023-05-07 15:23:06 +0900

    #23.05.07 level-0 commit

---
+++ level-0(2)/181917.js
@@ -0,0 +1,9 @@
+// 간단한 논리 연산
+// 문제 설명
+// boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때, 다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+
+// (x1 ∨ x2) ∧ (x3 ∨ x4)
+
+const solution = (x1, x2, x3, x4) => {
+  return (x1 || x2) && (x3 || x4);
+};
