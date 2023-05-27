00dc16ddf88fd1057b80a593faf7063d7361bd53
raccoonboy <raccoonboy0803@gmail.com>
2023-05-27 23:59:13 +0900

    #23.05.27 level-0 commit

---
+++ level-0(2)/120840.js
@@ -0,0 +1,29 @@
+// 구슬을 나누는 경우의 수
+// 문제 설명
+// 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ balls ≤ 30
+// 1 ≤ share ≤ 30
+// 구슬을 고르는 순서는 고려하지 않습니다.
+// share ≤ balls
+
+const factorial = (num) => {
+  if (num === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+const solution = (balls, share) => {
+  const factoTop = factorial(balls);
+  const factoBottom = factorial(balls - share);
+  const factoShare = factorial(share);
+  return Math.round(factoTop / (factoBottom * factoShare));
+};
+
+console.log(factorial(4));
+console.log(solution(3, 2));
+console.log(solution(5, 3));
