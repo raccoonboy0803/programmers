6573bddb99d7b142af28424ee17d6fa781455937
raccoonboy <raccoonboy0803@gmail.com>
2023-05-30 00:18:05 +0900

    #23.05.29 level-0 commit

---
+++ level-0(2)/181902.js
@@ -0,0 +1,24 @@
+// 문자 개수 세기
+// 문제 설명
+// 알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 1,000
+
+const solution = (my_string) => {
+  const arr = Array(52).fill(0);
+  return [...my_string].reduce((sum, cur) => {
+    let curIdx;
+    const curAlpha = cur.charCodeAt();
+    if (curAlpha >= 97) {
+      curIdx = curAlpha - 71;
+    } else {
+      curIdx = curAlpha - 65;
+    }
+
+    sum[curIdx]++;
+    return sum;
+  }, arr);
+};
+console.log(solution('Programmers'));
+console.log([...'Programmers']);
