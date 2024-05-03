fb90ae00f4e588372b59977acb292acf3a41993b
raccoonboy <112606000+raccoonboy0803@users.noreply.github.com>
2024-05-03 09:06:13 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,38 @@
+function solution(age) {
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  let result = [];
+
+  String(age)
+    .split('')
+    .map((v) => {
+      result.push(alphabet[parseInt(v)]);
+    });
+  return result.join('');
+}
