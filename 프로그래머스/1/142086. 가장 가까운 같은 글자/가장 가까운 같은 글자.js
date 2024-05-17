7a09e61779c29612ace8ba74ad41ff266ab74c4f
raccoonboy <112606000+raccoonboy0803@users.noreply.github.com>
2024-05-17 12:30:55 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 321.03 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,21 @@
+function solution(s) {
+  let result = [];
+  const splitS = s.split('');
+  splitS.forEach((item, index) => {
+    let findIndex = index;
+    if (splitS.indexOf(item) === index) {
+      result.push(-1);
+    } else {
+      for (let i = 0; i < splitS.length; i++) {
+        if (index === i) break;
+        if (splitS[i] === item) {
+          findIndex = i;
+        }
+      }
+
+      result.push(index - findIndex);
+    }
+  });
+
+  return result;
+}
