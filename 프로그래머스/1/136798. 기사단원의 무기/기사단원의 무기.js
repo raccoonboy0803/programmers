2c252a2aca7d0f984e4209e70ba9f53c35d0990a
raccoonboy <112606000+raccoonboy0803@users.noreply.github.com>
2024-05-23 10:16:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 7.62 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,28 @@
+function solution(number, limit, power) {
+  let divisorsArr = [];
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    divisorsArr.push(getDivisors(i).length);
+  }
+  console.log('divisorsArr::', divisorsArr);
+  divisorsArr.forEach((divisor) => {
+    if (divisor > limit) {
+      result += power;
+    } else {
+      result += divisor;
+    }
+  });
+
+  return result;
+
+  function getDivisors(num) {
+    const divisors = new Set();
+    for (let i = 1; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        divisors.add(i);
+        divisors.add(num / i);
+      }
+    }
+    return Array.from(divisors).sort((a, b) => a - b);
+  }
+}
